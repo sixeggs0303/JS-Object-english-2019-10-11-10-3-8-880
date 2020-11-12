826f9d9bf031600d79cb15f2d61c6d6e381a3215
Marcus Luk <marcus.luk@oocl.com>
2020-11-12 09:44:04 +0800

    finished tasks

---
+++ index.js
@@ -0,0 +1,17 @@
+// Task 1
+
+let user = {};
+user.name = "John";
+user.surname = "Mike";
+user.name = "Peter";
+delete user.name;
+
+// Task 2
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+
+console.log(fruit.apple + fruit.pear + fruit.peach);
